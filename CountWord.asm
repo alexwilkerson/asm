ef51ccf5dfaf25b2702cf8e3c8d34761b4aa6e8e
Alex Wilkerson <alex@uncorrected.com>
2016-10-19 10:16:47 -0500

    finished CountWord.asm

--- CountWord.asm
+++ CountWord.asm
@@ -1,26 +1,30 @@
 TITLE Count Word Program
 ; Program CountWord(CountWord.asm)
 ; This program computes the number of words in a user given input.
-; Last update : 10 / 18 / 16
+; Last update : 10 / 19 / 16
 ; Wilkerson, Alexander
 
 INCLUDE Irvine32.inc
 
 .data
-     UserInfo BYTE "This program iterates over a given string and counts the number of words inside of it.",0Dh,0Ah,0
-     PromptUser BYTE "Enter a string of words to be counted: ",0
-     Result BYTE "Number of words in string: ",0
+     PromptUser BYTE "Enter your string: ",0
+     Result BYTE "Your string had ",0
+     Single BYTE " word.",0
+     NotSingle BYTE " words.",0
+     RepeatPrompt BYTE "Repeat? (Y/N) ",0
+     NothingEntered BYTE "Nothing entered.",0
      
      buffer BYTE 500 DUP(0)             ; input buffer
      byteCount DWORD ?                  ; holds counter for buffer
 
+     char BYTE ?
+
+     numWords DWORD ?                   ; holds the number of words counted
+
 .code
 main PROC
+Beginning:
      call Clrscr
-
-     mov edx,OFFSET UserInfo
-     call WriteString
-     call Crlf
 
      mov edx,OFFSET PromptUser
      call WriteString
@@ -29,35 +33,67 @@ main PROC
      mov ecx, SIZEOF buffer             ; max size chars
      call ReadString                    ; get user input string
      mov byteCount, eax                 ; number of chars
-     call WriteString                   ; prints user input
-     call Crlf
+     cmp eax,0                          ; if nothing is entered
+     je NoInputError                    ; jump to print error
 
 ; put input string on stack
-     mov ecx,byteCount
-     mov esi,0
+     mov ecx,byteCount        ; size of input string for loop
+     mov esi,0                ; string iterator
      mov ebx,0                ; use ebx as boolean on char
      mov edx,0                ; use edx to count words
 
-L1:  movzx eax,buffer[esi]              ; get char
-     inc esi
-     cmp eax,32                         ; if the char is a space char
+L1:  movzx eax,buffer[esi]    ; get char
+     inc esi                  ; increase string iterator
+     cmp eax,32               ; if the char is a space char
      je L2
-     mov ebx,1
-     jmp L3
+     cmp ebx,0                ; if ebx boolean is 0
+     je L4
+     jmp L3                   ; else loop
 
-L2:  cmp ebx,1
-     jne L3
-     inc edx
-     mov ebx,0
-     jmp L3
+; if ebx boolean is not set
+L4:  inc edx        ; increase the word count
+     mov ebx,1      ; set ebx boolean
+     jmp L3         ; loop
+
+; change ebx boolean to 0
+L2:  mov ebx,0
 
 L3:  loop L1
-     
-     mov eax,edx
-     call WriteDec
-     call Crlf
-     call WaitMsg
 
-     exit
+     mov numWords,edx    ; store the number of words
+     
+; write the result of how many words there are
+     mov edx,OFFSET Result
+     call WriteString
+     mov eax,numWords
+     call WriteDec
+     cmp eax,1                ; if count is 1
+     je SingleWord            ; print word instead of words
+     jne NotSingleWord        ; else print words
+SingleWord:
+     mov edx,OFFSET Single    ; word
+     jmp WriteOut
+NotSingleWord:
+     mov edx,OFFSET NotSingle ; words
+WriteOut:
+     call WriteString
+     call Crlf
+RepeatLabel:
+     mov edx,OFFSET RepeatPrompt   ; prompt user to repeat
+     call WriteString
+     call ReadChar       ; read single key user input
+     cmp al,'Y'          ; if Y
+     je Beginning        ; restart
+     cmp al,'y'          ; if y
+     je Beginning        ; restart
+
+     exit                ; else, exit
+
+NoInputError:
+     mov edx,OFFSET NothingEntered
+     call WriteString
+     call Crlf
+     jmp RepeatLabel
+
 main ENDP
 END main
